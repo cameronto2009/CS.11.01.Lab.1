ffd6d9b80bc6df93fea1df8954b00278d8c6cb77
cameron <cameront2027@student.cis.edu.hk>
2024-08-29 18:01:08 +0800

    first commit

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your age please!: ");
+        int age = -1;
+        while(age < 0) {
+            try {
+                age = Integer.parseInt(scan.nextLine());
+                if(age < 0){
+                    System.out.println("Hi please enter a correct number!");
+                }
+            }
+            catch(NumberFormatException e){
+                System.out.println("Please enter a correct number");
+            }
+        }
+        System.out.println(age + " years old should be dating somebody who is at least " + (age/5+9) + " years old.");
+
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your age please: ");
+        int age = -1;
+        while(age < 0) {
+            try {
+                age = Integer.parseInt(scan.nextLine());
+            }
+            catch(NumberFormatException e){
+                System.out.println("Please enter a correct numbre!, weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeelo");
+            }
+        }
+        System.out.println(age + " years old should read at least " + (100-age) + " before playing video games!.");
 
     }
 }
